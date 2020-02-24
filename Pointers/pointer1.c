21aeacc467e3a6e4a61b27050a34e0e47d390d4f
Meliih <melihkahraman4@gmail.com>
2020-02-24 14:48:31 +0300

    upload projects

---
+++ Pointers/pointer1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+	int main () {
+		
+	/*	int var1=5;
+		char var2;
+		
+		printf("var1 degiskenini adresi : %p\n",&var1);
+		printf("var2 degiskenini adresi : %p\n",&var2);
+		printf ("Adres: %p\t Deger: %d\n", &var1, var1 );
+	*/
+		
+
+	/*
+		int  var1[6] = {2, 4, 8, 16, 32, 64};
+		for (int i = 0; i <= 5; i++)
+        printf("Adres: %p\t Deger: %d\n",(void*)&var1[i], var1[i] );
+     */
+        
+        
+	/*	char  var1[7] = "merhaba";
+		for (int i = 0; i <= 6; i++)
+        printf("Adres: %p\t Deger: %c\n",(void*)&var1[i], var1[i] );
+	*/
+	
+	//	printf("Adres : %p",&main);
+	
+	/*	int a = 5;
+		int *ptr;
+		ptr = &a;
+		printf("Adres: %p  Deger: %d \n", &a, a);
+		printf("Adres: %p  Deger: %d \n", ptr, *ptr);
+	*/
+		
+	/*	int  *ptr; 
+		ptr = NULL;
+		printf("Adres: %p \n", ptr);
+	*/
+	
+	/*	int  **t,*z,y = 10;
+		z = &y;
+		t = &z;
+		printf ("%p, %p, %p\n", t, &z, &y);
+	*/
+	
+	/*	char dizi[5]="melih";
+		char *ptr=&dizi[0];
+		
+		for (int i =0;i<5;i++)
+			printf("Adres : %p Degeri : %c\n",ptr+i,*(ptr+i));
+	*/
+	
+			
+	/*	char  msg[7] = "merhaba";
+		char  *ptr = &msg[0];    
+  
+		for (int i = 0; i <= 6; i++)
+		{
+        printf("Adres: %p\t Deger: %c\n", ptr, *ptr );
+        ptr++;
+		}
+		
+    */   
+        
+   return 0;
+	}
---
+++ Pointers/pointer2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+
+int main () {
+/*	 
+	char*gunler [] ={"pazartezi","sali","carsamba","persembe","cuma","cumartesi","pazar"};
+	int i=0;
+	while (i<7) {
+	printf("%s\n",gunler[i]);
+	i++;
+	}
+	
+	*/
+/*
+	int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int *pa = a;        
+	
+		for (; pa <= &a[9] ; pa++)
+		{
+		printf("%d\n", *pa);
+		} 
+		
+*/
+/*
+		int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		for (int i = 0; i < 10 ; i++){
+		printf("%d %d\n", a[i], *(a + i));
+		} 
+	*/
+	
+	
+	/*
+	char  m[8] = "merhaba";
+	char  *pm = &m[0];    
+
+	for (; *pm != '\0'; pm++){
+     printf("%c", *pm);
+	} 
+*/
+
+/*
+	char t[5]="melih";
+	char  *pm2 = &t[0];    
+		for (int i = 0;i<5; i++){
+		printf("%c %c %c %c\n",*(pm2+i), pm2[i],*(t + i), t[i]);
+		}
+*/
+
+/*
+	char  m[7] = "merhaba";
+	char  *pm = &m[0]; 
+	printf("%.15s \n%.15s \n%.15s\n", &m[0], m, pm);
+
+*/
+/*
+    int a[2][3] = {{1, 2, 3}, 
+            {10, 20, 30}};
+    int *b[2];
+
+    b[0] = a[0];
+    b[1] = a[1];
+
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 3; j++){
+            printf("%d \t", b[i][j]);
+        }
+        printf("\n");
+    }
+*/
+
+return 0;
+}
